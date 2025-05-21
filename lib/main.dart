a2a59a315fac9ec81145da1e4756b239fea7ecc1
Andrew <122733974+AkiAndrew@users.noreply.github.com>
2025-05-21 23:41:15 +0800

    Minimized 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:assignment/user_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Firebase Core
 import 'scanner_screen.dart';
-import 'inventory_screen.dart';
-import 'expiry_tracker_screen.dart';
 import 'waste_reduction_screen.dart';
-import 'recommendations_screen.dart';
-import 'item_screen.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
 import 'database_screen.dart';
@@ -29,17 +25,12 @@ class FridgeApp extends StatelessWidget {
       routes: {
         '/': (context) => HomeScreen(),
         '/scanner': (context) => ScannerScreen(),
-        '/inventory': (context) => InventoryScreen(),
-        '/expiry': (context) => ExpiryTrackerScreen(),
         '/waste': (context) => WasteReductionScreen(),
-        '/recommendations': (context) => RecommendationsScreen(),
-        '/item': (context) => ItemScreen(),
-        '/login': (context) => LoginScreen(), // add login route
+        '/login': (context) => LoginScreen(), 
         '/admin_home_screen': (ctx) => AdminHomeScreen(),
         '/user_home_screen': (ctx) => UserHomeScreen(),
         '/database': (ctx) => DatabaseScreen(),
         '/product': (context) =>  ProductScreen(),
-
       },
     );
   }
